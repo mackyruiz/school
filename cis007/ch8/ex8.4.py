73d11a89ff240c37238c3554fdfd81c18bf1f942
Macky Ruiz <ruiz.mackyjo@gmail.com>
2019-01-16 14:31:36 -0800

    Ch8
    WIP

---
+++ cis007/ch8/ex8.4.py
@@ -0,0 +1,16 @@
+# Ch5 ex 8.4
+# Macky Ruiz
+# CIS 007
+
+#function that finds the # of occurences of a special character in a string:
+# def count(s, ch):
+
+# str class has the count method. implement method w/o using the count method
+# ex: count("Welcome", 'e') returns 2. Write a program that prompts the user
+# to enter a string followed by a character and displays the # of occurences
+# of the character in the strong
+
+# >>> print(str.count("Welcome", 'e'))
+# 2
+
+def count(s, ch):
