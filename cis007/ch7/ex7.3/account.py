1fdf3e6788bbdf2262665a22cfb498cbde3e74b0
Macky Ruiz <ruiz.mackyjo@gmail.com>
2019-01-16 14:13:34 -0800

    ex7.3
    Completion of ex7.3 class and function

--- cis007/ch7/ex7.3/account.py
+++ cis007/ch7/ex7.3/account.py
@@ -34,8 +34,9 @@ withdraw(ammount: float)
 class Account:
     def __init__(self, accountID=0, balance=100, annualInterestRate=0):
         self.__accountID=accountID
-        self.__balance=bal
-        self._annualInterestRate=annualInterestRate
+        self.__balance=balance
+        self.__annualInterestRate=annualInterestRate
+
     def getAccountID(self):
         return self.__accountID
 
@@ -43,14 +44,17 @@ class Account:
         self.__accoundID = accoundID
 
     def getMonthlyInterestRate(self):
-        self.__aInterestRate/12
+        self.__annualInterestRate/12
 
     def getMonthlyInterest(self):
-        ammount = self.__balance * aInterestRate/12
-        return amount
+        ammount = self.__balance * self.__annualInterestRate/12
+        return ammount
 
     def deposit(self, ammount):
         self.__balance += ammount
 
     def withdraw(self, ammount):
         self.__balance -= ammount
+
+    def getBalance(self):
+        return self.__balance
--- cis007/ch7/ex7.3/main.py
+++ cis007/ch7/ex7.3/main.py
@@ -2,13 +2,13 @@
 # main function
 # Macky Ruiz
 # CIS 007
-from account import Account
+from Account import Account
 
 def main():
-    a1 = account() #Creates an account with default values
-    acc = account(1122, 20000, 0.045)
+    a1 = Account() #Creates an account with default values
+    acc = Account(1122, 20000, 0.045)
     acc.withdraw(2500)
     acc.deposit(3000)
-    print(acc.getAccountID(), acc.getBalance(), getMonthlyInterestRate(), acc.getMonthlyInterest())
+    print(acc.getAccountID(), acc.getBalance(), acc.getMonthlyInterestRate(), acc.getMonthlyInterest())
 
 main()
