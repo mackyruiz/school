3795cefc8f8aaad89217defcf7ede24efdcdfbd1
Macky Ruiz <ruiz.mackyjo@gmail.com>
2019-01-16 16:26:40 -0800

    Update main.py

--- cis007/ch7/ex7.5/main.py
+++ cis007/ch7/ex7.5/main.py
@@ -9,7 +9,6 @@ def main():
     polygon1 = RegularPolygon()
     polygon2 = RegularPolygon(6, 4)
     polygon3 = RegularPolygon(10, 4, 5.6, 7.8)
-
     print("Polygon1's Perimeter is: ", format(polygon1.getPerimeter(), ".2f"), " and Area is ", format(polygon1.getArea(), ".2f"))
     print("Polygon2's Perimeter is: ", format(polygon2.getPerimeter(), ".2f"), " and Area is ", format(polygon2.getArea(), ".2f"))
     print("Polygon3's Perimeter is: ", format(polygon3.getPerimeter(), ".2f"), " and Area is ", format(polygon3.getArea(), ".2f"))
